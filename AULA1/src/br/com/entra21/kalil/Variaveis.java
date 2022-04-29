457f47214b587e0f7ebee75ed908e792650ec9fc
kaka-jaques <kjfakhouri@gmail.com>
2022-04-29 18:56:13 -0300

    AULA 1 - VARIAVEIS, CONSTANTES E COMENTÁRIOS

---
+++ AULA1/src/br/com/entra21/kalil/Variaveis.java
@@ -0,0 +1,23 @@
+package br.com.entra21.kalil;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		//Variáveis FINAIS & Tipos de variáveis e seus tamanhos - AULA 2
+		
+		 System.out.println("\n****VARIABLES INFORMATION****\n");
+		 System.out.println("The Value of 'Byte' is: "+Byte.MIN_VALUE+" - "+Byte.MAX_VALUE);
+		 System.out.println("The Value of 'Short' is: "+Short.MIN_VALUE+" - "+Short.MAX_VALUE);
+		 System.out.println("The Value of 'Int' is: "+Integer.MIN_VALUE+" - "+Integer.MAX_VALUE);
+		 System.out.println("The Value of 'Double' is: "+Double.MIN_VALUE+" - "+Double.MAX_VALUE);
+		 System.out.println("The Value of 'Float' is: "+Float.MIN_VALUE+" - "+Float.MAX_VALUE);
+		 System.out.println("The Value of 'Long' is: "+Long.MIN_VALUE+" - "+Long.MAX_VALUE+"\n");
+		
+		 
+		 
+		 
+	}
+	
+
+}
